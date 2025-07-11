73e6a0ccda834b0de5f307c3cdfcc038c8acf869
chennethelius <97802838+chennethelius@users.noreply.github.com>
2025-07-11 11:51:35 -0500

    added firestore access
    now adds user to firestore on register

--- flutter_auth/lib/services/auth_service.dart
+++ flutter_auth/lib/services/auth_service.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 /// Simplified AuthService that works on mobile and web
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   /// Triggers Google Sign-In flow
   /// Returns a Firebase [User] on success or null on cancel/error
@@ -28,6 +31,31 @@ class AuthService {
 
       // Sign in to Firebase with the Google credential
       final userCredential = await _auth.signInWithCredential(credential);
+
+      // make sure we have a user
+      final user = userCredential.user;
+      if (user == null) return null;
+
+    // add user to firestore user collection
+      final userDoc = _firestore.collection('users').doc(user.uid);
+
+      // parses users name into first and last
+      final fullName = user.displayName ?? '';
+      final nameParts = fullName.split(' ');
+      final firstName = nameParts.isNotEmpty ? nameParts.first : '';
+      final lastName  = nameParts.length > 1 ? nameParts.sublist(1).join(' ') : '';
+
+    // adds user to firestore collection users
+      await userDoc.set({
+        'firstName':     firstName,
+        'lastName':      lastName,
+        'email':         user.email       ?? '',
+        'role':          'student',                  
+        'pointsBalance': 0,                          
+        'createdAt':     FieldValue.serverTimestamp(),
+      });
+
+
       return userCredential.user;
     } catch (e) {
       //print('❌ Google sign-in error: $e');
